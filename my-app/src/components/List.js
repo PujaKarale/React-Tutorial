2f37a8d1da524492cf3bd87cf243cdf685ccbf51
pooja <pooja.karale@polyglots.io>
2024-02-14 14:22:51 +0530

    getting input value on button click

---
+++ my-app/src/components/List.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+//getting input box value:= on button click
+
+export default function List() {
+ const[val, setVal]=useState(null)
+ const[print, setPrint]=useState(false)
+
+   function  getData(data){
+   setVal( data.target.value)
+   console.log(val)
+   }
+  return (
+    <div>
+      <input type="text"  placeholder='enter your name' onChange={getData}/>
+      <button onClick={()=>setPrint(true)}>Update Data</button>
+      {/* if print has value then print h1 otherwise null */}
+      {print ? <h3>{val}</h3> : null}
+      
+    </div>
+  )
+}
