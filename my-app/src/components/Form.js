3fb3c45e27d09001e8e7621c4ceb4ccaa3f7cddf
pooja <pooja.karale@polyglots.io>
2024-02-15 23:23:48 +0530

    Adding basic form validations

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,3 +1,4 @@
+import { getQueriesForElement } from '@testing-library/react'
 import React from 'react'
 import { useState } from 'react'
 
@@ -32,7 +33,70 @@ const[interest, setInterest]=useState("")
 
 
         </form>
-      
+        
+      <FormValidations/>
     </div>
-  )
-}
+    )
+  }
+
+
+
+  function FormValidations(){
+  
+    const[name, setName]=useState("")
+    const[pass, setPass]=useState("")
+    const[userErr, setUserErr]=useState(false)
+    const[passErr, setPassErr]=useState(false)
+
+
+   function onFormSubmit(e){
+   e.preventDefault()
+   if(name.length <3  || pass.length <3) {//updated values la validations add kele
+       alert("enter valid data")
+   }else{
+      alert(" all good")
+   }
+    }
+
+  
+   function userHandler(e){
+        console.log(e.target.value.length)
+        let item= e.target.value;
+        if(item.length<3){
+          
+          setUserErr(true)
+        }else{
+          
+          setUserErr(false)
+        }
+       setName(item) // name sathi state set keli
+
+   }
+
+
+  function  passHandler(e){
+    let item= e.target.value;
+   if(item.length < 2){
+  setPassErr(true)
+   }else{
+  setPassErr(false)
+   
+  }
+   setPass(item)  // password sathi state set keli
+  }
+
+
+
+    return(
+      <div>
+     <h4> Adding basic Validations to Form</h4>
+    <form onSubmit={onFormSubmit}>
+    <input type="text"  placeholder='enter your name' onChange={userHandler} />
+     {userErr ? <span> Enter Valid Credentials</span> : null} <br /><br />
+    <input type="password" placeholder='enter your password'  onChange={passHandler}  />
+    {passErr ? <span> Enter Valid password</span> : null}<br /><br />
+      <button type ="submit">Submit</button>
+      </form>
+      </div>
+    )
+  }
